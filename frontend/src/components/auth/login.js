46631ab40d777f6106f51ef37a2e33b45ed45d4d
Diana Arazklycheva <arazklycheva94@mail.ru>
2024-05-21 21:05:06 +0500

    внесение правок по замечаниям

--- frontend/src/components/auth/login.js
+++ frontend/src/components/auth/login.js
@@ -1,5 +1,24 @@
 export class Login {
     constructor() {
-        console.log('LOGIN');
+        this.emailElement = document.getElementById('email');
+        this.passwordElement = document.getElementById('password');
+        document.getElementById('process-button').addEventListener('click', this.login.bind(this));
+    }
+
+    validateForm() {
+        if (this.emailElement.value && this.emailElement.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+            this.emailElement.classList.remove('is-invalid');
+        } else {
+            this.emailElement.classList.add('is-invalid');
+        }
+
+        if (this.passwordElement.value) {
+            this.passwordElement.classList.remove('is-invalid');
+        } else {
+            this.passwordElement.classList.add('is-invalid');
+        }
+    }
+    login() {
+        this.validateForm();
     }
 }
--- frontend/src/components/auth/sign-up.js
+++ frontend/src/components/auth/sign-up.js
@@ -1,5 +1,39 @@
 export class SignUp {
     constructor() {
-        console.log('SIGN-UP');
+        this.nameElement = document.getElementById('name');
+        this.emailElement = document.getElementById('email');
+        this.passwordElement = document.getElementById('password');
+        this.passwordRepeatElement = document.getElementById('password-repeat');
+        document.getElementById('process-button').addEventListener('click', this.signUp.bind(this));
+    }
+
+    validateForm() {
+        if (this.nameElement.value) {
+            this.nameElement.classList.remove('is-invalid');
+        } else {
+            this.nameElement.classList.add('is-invalid');
+        }
+
+        if (this.emailElement.value && this.emailElement.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+            this.emailElement.classList.remove('is-invalid');
+        } else {
+            this.emailElement.classList.add('is-invalid');
+        }
+
+        if (this.passwordElement.value && this.passwordElement.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
+            this.passwordElement.classList.remove('is-invalid');
+        } else {
+            this.passwordElement.classList.add('is-invalid');
+        }
+
+        if(this.passwordRepeatElement.value && this.passwordRepeatElement.value === this.passwordElement.value) {
+            this.passwordRepeatElement.classList.remove('is-invalid');
+        } else {
+            this.passwordRepeatElement.classList.add('is-invalid');
+        }
+    }
+
+    signUp() {
+        this.validateForm();
     }
 }

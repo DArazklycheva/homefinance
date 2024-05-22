23ed51d77c913cf3aa2190643c91362fcc91af9e
Diana Arazklycheva <arazklycheva94@mail.ru>
2024-05-22 10:50:17 +0500

    внесение правок для работы с error-input в login и signup

--- frontend/src/components/auth/login.js
+++ frontend/src/components/auth/login.js
@@ -2,20 +2,26 @@ export class Login {
     constructor() {
         this.emailElement = document.getElementById('email');
         this.passwordElement = document.getElementById('password');
+        this.emailSvgElement = document.getElementById('email-svg');
+        this.passwordSvgElement = document.getElementById('password-svg');
         document.getElementById('process-button').addEventListener('click', this.login.bind(this));
     }
 
     validateForm() {
         if (this.emailElement.value && this.emailElement.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
             this.emailElement.classList.remove('is-invalid');
+            this.emailSvgElement.classList.remove('error-input');
         } else {
             this.emailElement.classList.add('is-invalid');
+            this.emailSvgElement.classList.add('error-input');
         }
 
         if (this.passwordElement.value) {
             this.passwordElement.classList.remove('is-invalid');
+            this.passwordSvgElement.classList.remove('error-input');
         } else {
             this.passwordElement.classList.add('is-invalid');
+            this.passwordSvgElement.classList.add('error-input');
         }
     }
     login() {
--- frontend/src/components/auth/sign-up.js
+++ frontend/src/components/auth/sign-up.js
@@ -4,32 +4,44 @@ export class SignUp {
         this.emailElement = document.getElementById('email');
         this.passwordElement = document.getElementById('password');
         this.passwordRepeatElement = document.getElementById('password-repeat');
+        this.nameSvgElement = document.getElementById('name-svg');
+        this.emailSvgElement = document.getElementById('email-svg');
+        this.passwordSvgElement = document.getElementById('password-svg');
+        this.passwordRepeatSvgElement = document.getElementById('password-repeat-svg');
         document.getElementById('process-button').addEventListener('click', this.signUp.bind(this));
     }
 
     validateForm() {
         if (this.nameElement.value) {
             this.nameElement.classList.remove('is-invalid');
+            this.nameSvgElement.classList.remove('error-input');
         } else {
             this.nameElement.classList.add('is-invalid');
+            this.nameSvgElement.classList.add('error-input');
         }
 
         if (this.emailElement.value && this.emailElement.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
             this.emailElement.classList.remove('is-invalid');
+            this.emailSvgElement.classList.remove('error-input');
         } else {
             this.emailElement.classList.add('is-invalid');
+            this.emailSvgElement.classList.add('error-input');
         }
 
         if (this.passwordElement.value && this.passwordElement.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
             this.passwordElement.classList.remove('is-invalid');
+            this.passwordSvgElement.classList.remove('error-input');
         } else {
             this.passwordElement.classList.add('is-invalid');
+            this.passwordSvgElement.classList.add('error-input');
         }
 
         if(this.passwordRepeatElement.value && this.passwordRepeatElement.value === this.passwordElement.value) {
             this.passwordRepeatElement.classList.remove('is-invalid');
+            this.passwordRepeatSvgElement.classList.remove('error-input');
         } else {
             this.passwordRepeatElement.classList.add('is-invalid');
+            this.passwordRepeatSvgElement.classList.add('error-input');
         }
     }
 
